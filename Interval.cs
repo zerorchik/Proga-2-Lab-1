88deea24b4058ff5b7fd8951c5fbc4be77045441
zerorchik <s.pavlova.ic12@kpi.ua>
2022-04-29 02:06:58 +0300

    Labka 1

---
+++ Interval.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace proga_2_lab_1
+{
+    public class Interval
+    {
+        private double _start;
+        private double _end;
+
+        public double Start { get { return _start; } }
+        public double End { get { return _end; } }
+
+        public Interval(double start, double end)
+        {
+            if (start > end)
+            {
+                _start = end;
+                _end = start;
+            }
+            else
+            {
+                _start = start;
+                _end = end;
+            }
+        }
+
+        public static double Length(Interval interval)
+        {
+            return interval.End - interval.Start;
+        }
+
+        public static Interval Move(Interval interval, double a)
+        {
+            return new Interval(interval.Start + a, interval.End + a);
+        }
+
+        public static Interval MultiplyToNum(Interval interval, double a)
+        {
+            return new Interval(interval.Start * a, interval.End * a);
+        }
+
+        public static Interval Sum(Interval interval1, Interval interval2)
+        {
+            return new Interval(interval1.Start + interval2.Start, interval1.End + interval2.End);
+        }
+
+        public static Interval Difference(Interval interval1, Interval interval2)
+        {
+            return new Interval(interval1.Start - interval2.Start, interval1.End - interval2.End);
+        }
+
+        public static bool IsBigger(Interval interval1, Interval interval2)
+        {
+            if (Length(interval1) > Length(interval2))
+                return true;
+            else return false;
+        }
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+        public void SaveJson(string filePath)
+        {
+            File.WriteAllText(filePath, ToJson());
+        }
+
+        public static Interval FromJsonFile(string filepath)
+        {
+            return JsonConvert.DeserializeObject<Interval>(File.ReadAllText(filepath));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static proga_2_lab_1.Interval;
+
+namespace proga_2_lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Default;
+
+            Interval a = new Interval(-4, 7);
+            Interval b = new Interval(3, 9);
+
+            Console.WriteLine("Інтервал А = (" + a.Start + ", " + a.End + ")");
+            Console.WriteLine("Інтервал B = (" + b.Start + ", " + b.End + ")");
+            Console.WriteLine("\n------------------\n\nДОВЖИНА:\n|А| = " + Length(a));
+            Console.WriteLine("|B| = " + Length(b));
+
+            if (IsBigger(a, b))
+                Console.WriteLine("\nПОРІВНЯННЯ:\nA > B");
+            else Console.WriteLine("\nПОРІВНЯННЯ:\nA < B");
+
+            Console.WriteLine("\nЗСУВ У ПРАВО:\nA + 1 = (" + Move(a, 1).Start + ", " + Move(a, 1).End + ")");
+            Console.WriteLine("\nЗСУВ У ЛІВО:\nВ - 0.5 = (" + Move(b, -0.5).Start + ", " + Move(b, -0.5).End + ")");
+
+            Console.WriteLine("\nСТИСНЕННЯ:\nA / 2 = (" + MultiplyToNum(a, 0.5).Start + ", " + MultiplyToNum(a, 0.5).End + ")");
+            Console.WriteLine("\nРОЗШИРЕННЯ:\nВ * 1.5 = (" + MultiplyToNum(b, 1.5).Start + ", " + MultiplyToNum(b, 1.5).End + ")");
+
+            Console.WriteLine("\nСУМА:\nA + В = (" + Sum(a, b).Start + ", " + Sum(a, b).End + ")");
+            Console.WriteLine("\nРІЗНИЦЯ:\nА - В = (" + Difference(a, b).Start + ", " + Difference(a, b).End + ")");
+
+            var toJson = a.ToJson();
+            a.SaveJson("aJson.json");
+            Interval fromJson = Interval.FromJsonFile("aJson.json");
+
+            Console.WriteLine("\nЗБЕРЕЖЕННЯ У JSON:\nA -> JSON: \n" + toJson);
+            Console.WriteLine("\nЗЧИТУВАННЯ З ФАЙЛА JSON:\nJSON -> A = (" + fromJson.Start + ", " + fromJson.End + ")");
+        }
+    }
+}
